8e8770af3f344131aecf744fc7e92d3f4affa4a4
Ulrich Dobramysl <1979498+ulido@users.noreply.github.com>
2024-05-23 10:37:47 +0000

    Add slapassign.

---
+++ src/slapquant/assign.py
@@ -0,0 +1,83 @@
+import itertools
+import geffa
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+def assign_sites(gene_models_gff, slas_pas_sites_gff):
+    gene_models = geffa.GffFile(gene_models_gff, ignore_unknown_feature_types=True)
+    slas_pas = geffa.GffFile(slas_pas_sites_gff)
+
+    processed_regions = set()
+    for seqreg in gene_models.sequence_regions.values():
+        try:
+            sitesreg = slas_pas.sequence_regions[seqreg.name]
+        except KeyError:
+            logger.warning(f"SLAS/PAS sites GFF does not contain sequence region {seqreg.name} - possible mismatch between slapquant run and given gene models?")
+            continue
+        for SLAS in (feature for feature in sitesreg.node_registry.values() if feature.type == 'SLAS'):
+            geffa.geffa.SLASNode(SLAS.line_nr, seqreg, SLAS.source, 'SLAS', SLAS.start, SLAS.end, '.', SLAS.strand, '.', f"ID={SLAS.attributes['ID']};usage={SLAS.attributes['usage']}")
+        for PAS in (feature for feature in sitesreg.node_registry.values() if feature.type == 'PAS'):
+            geffa.geffa.PASNode(PAS.line_nr, seqreg, PAS.source, 'PAS', PAS.start, PAS.end, '.', PAS.strand, '.', f"ID={PAS.attributes['ID']};usage={PAS.attributes['usage']}")
+        processed_regions.add(seqreg.name)
+    sites_only_regions = set(slas_pas.sequence_regions).difference(processed_regions)
+    if len(sites_only_regions) > 0:
+        logger.warning(f"Gene models GFF does not contain sequence regions present in the SLAS/PAS sites GFF - possible mismatch between slapquant run and given gene models?")
+
+    for seqreg in gene_models.sequence_regions.values():
+        for slas in (node for node in seqreg.node_registry.values() if node.type == 'SLAS'):
+            nodes_to_search = sorted(
+                (
+                    feature for feature in seqreg.node_registry.values()
+                    if (
+                        feature.type in ['CDS', 'PAS'] and
+                        (
+                            (
+                                (slas.strand == '+') and (feature.strand == '+') and (feature.start >= slas.end) or
+                                (slas.strand == '-') and (feature.strand == '-') and (feature.end <= slas.start)
+                            )
+                        )
+                    )
+                ),
+                key=lambda x: x.start if slas.strand == '+' else -x.end
+            )
+            if len(nodes_to_search) == 0:
+                logger.info(f"No CDS found we could match {slas.attributes['ID']} to. Skipping.")
+                continue
+
+            closest_node = nodes_to_search[0]
+
+            if closest_node.type == 'PAS':
+                logger.info(f"Closest node to {slas.attributes['ID']} is a PAS, no CDS could be assigned.")
+            else:
+                slas.add_parent(closest_node.parents[0].parents[0])
+
+        for pas in (node for node in seqreg.node_registry.values() if node.type == 'PAS'):
+            nodes_to_search = sorted(
+                (
+                    feature for feature in seqreg.node_registry.values()
+                    if (
+                        feature.type in ['CDS', 'SLAS'] and
+                        (
+                            (
+                                (pas.strand == '+') and (feature.strand == '+') and (feature.end <= pas.start) or
+                                (pas.strand == '-') and (feature.strand == '-') and (feature.start >= pas.end)
+                            )
+                        )
+                    )
+                ),
+                key=lambda x: -x.end if pas.strand == '+' else x.start
+            )
+            if len(nodes_to_search) == 0:
+                logger.info(f"No CDS found we could match {pas.attributes['ID']} to. Skipping.")
+                continue
+
+            closest_node = nodes_to_search[0]
+
+            if closest_node.type == 'SLAS':
+                logger.info(f"Closest node to {pas.attributes['ID']} is a SLAS, no CDS could be assigned.")
+            else:
+                pas.add_parent(closest_node.parents[0].parents[0])
+
+    return gene_models
--- src/slapquant/cli.py
+++ src/slapquant/cli.py
@@ -3,8 +3,9 @@ import pathlib
 import argparse
 
 from slapquant.slapquant import process_reads
+from slapquant.assign import assign_sites
 
-def main():
+def slapquant_main():
     parser = argparse.ArgumentParser(description="Find spliced leader acceptor and polyadenylation sites by aligning high-quality RNASeq reads to an existing genome. Note that the reads net to be trimmed beforehand.")
     parser.add_argument('reference_genome', type=pathlib.Path, help="""The path to a FASTA file containing the reference genome used to align the RNASeq reads to.""")
     parser.add_argument('rnaseq_reads', nargs='+', type=pathlib.Path, help="""The path(s) to (potentially multiple) FASTQ files containing the RNASeq reads. Note that no special handling for paired-end reads is done.""")
@@ -15,4 +16,16 @@ def main():
     logging.basicConfig(filename='/dev/stderr', level=args.loglevel)
 
     gff = process_reads(args.reference_genome, args.rnaseq_reads)
+    gff.save('/dev/stdout')
+
+def slapassign_main():
+    parser = argparse.ArgumentParser(description="Assign spliced leader accaptor and polyadenylation sites to gene models. The resulting GFF file contains the sites assigned to the given gene models and is printed to standar output.")
+    parser.add_argument('gene_models_gff', type=pathlib.Path, help="The path to a GFF file containing the gene models the sites should be matched to. Please note that this must correspond to the original genome slapquant was run on to identify the sites.")
+    parser.add_argument('slas_pas_gff', type=pathlib.Path, help="The path to a GFF file containing the spliced leader accaptor and polyadenylation sites identified from a prior slapquant run.")
+    parser.add_argument('-v', '--verbose', help="""Give more info about the process.""", action="store_const", dest="loglevel", const=logging.INFO, default=logging.WARNING)
+
+    args = parser.parse_args()
+    logging.basicConfig(filename='/dev/stderr', level=args.loglevel)
+    
+    gff = assign_sites(args.gene_models_gff, args.slas_pas_gff)
     gff.save('/dev/stdout')
