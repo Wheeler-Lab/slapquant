ab259b00aa4abc9f778a06d808a7a508d6638d9a
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2025-07-07 21:42:28 +0100

    Revert default --min-pas-usage and --min-slas-usage to 4
    Reverts 15be36f, which was an unnecessary change due to an outstanding bug fixed in 5b76692

--- src/slapquant/cli.py
+++ src/slapquant/cli.py
@@ -255,20 +255,20 @@ def slapassign_main():
         help=(
             "When assigning sites to genes, if SLAS sites are between a "
             "CDS and a site to be assigned, only consider those with a "
-            "minimum usage count. (default 2)"
+            "minimum usage count. (default 4)"
         ),
         type=int,
-        default=2,
+        default=4,
     )
     parser.add_argument(
         '--min-pas-usage', '-n',
         help=(
             "When assigning sites to genes, if PAS sites are between a "
             "CDS and a site to be assigned, only consider those with a "
-            "minimum usage count. (default 1)"
+            "minimum usage count. (default 4)"
         ),
         type=int,
-        default=1,
+        default=4,
     )
     parser.add_argument(
         '-v',
