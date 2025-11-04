4cc9b69ba55f224b8d2ad0c5419177d7c1e6fc75
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2025-11-04 14:39:04 +0000

    Fix: Prevent ORF lengthening assigning than zero-length 5' UTRs

--- src/slapquant/assign.py
+++ src/slapquant/assign.py
@@ -330,7 +330,7 @@ def identify_UTRs(
                     )
                     if fix_lengthen_orfs:
                         best_offset = -1
-                        for i in range(len(utr.sequence), 0, -3):
+                        for i in range(len(utr.sequence), 2, -3):
                             if utr.sequence[i-3:i] in ["TAA", "TGA", "TAG"]:
                                 best_offset = -1
                                 break
